16d2ba1bea5f822c08fc8f226db89c55c85926e6
Deadhead84 <Jon@Scoville-MacBook-3.local>
2015-01-29 11:16:22 -0700

    completed

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,13 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+
+String.prototype.reversed = function(a) {
+	console.log(a.split("").reverse().join(""))
+}
+
+var test = "Will this work?";
+test.reversed(test);
+
+var newCar = "2007 Subaru Outback";
+newCar.reversed(newCar);
--- quizApp.js
+++ quizApp.js
@@ -7,26 +7,41 @@
 
   //code here
 
+var User = function(name, email, password, totalScore) {
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+}
+
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
+var Question = function(title, answersArray, rightAnswer, difficulty) {
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+	this.difficulty = difficulty;
 
+}
 
 //Create a users Array which is going to hold all of our users.
 
   //code here
-
+var users = [];
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
 
-
+users.push(new User("Jon", "j@js.com", "password", ""));
+users.push(new User("Tiff", "tif@ts.com", "passwrod", ""));
+users.push(new User("Sophie", "sohpie@ss", "password", ""));
 //Create a questions Array which is going to hold all of our questions
 
   //code here
-
+var questions = [];
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -34,15 +49,26 @@
 //title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
 //Fill in the rest of the required data as you see appropriate.
 
-  //code here
+  //code 
+var first = new Question("first:", "T/F: Inheritance is achieved in Javascript through Prototypes?");
+var second = new Question("second:", "T/F: JavaScript is just a scripting version of Java");
+var third = new Question("third:", "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value");
 
 
 //Now push all of your instances of Question into the questions Array
 
   //code here
-
+questions.push(first, second, third);
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
   //code here
+
+  for (i in users) {
+  	console.log(users[i].name);
+  }
+
+  for (i in questions) {
+  	console.log(i);
+  }
 
